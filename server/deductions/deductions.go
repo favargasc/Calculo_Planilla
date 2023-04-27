d49d95e59cea85e15056ae78831087f98cd214f5
fabian <favargasc@estudiantec.cr>
2023-04-27 04:33:47 -0600

    feat: rutas para las deduciones obreras y patronales

--- server/deductions/deductions.go
+++ server/deductions/deductions.go
@@ -8,6 +8,16 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+type Deduction struct {
+	Period         string  `db:"period"`
+	Status         bool    `db:"status"`
+	Id             int     `db:"id"`
+	Concept        string  `db:"concept"`
+	Percentage     float32 `db:"percentage"`
+	EffectiveDate  string  `db:"effective_date"`
+	ExpirationDate string  `db:"expiration_date"`
+}
+
 func CalculateEmployerDeduction(c *fiber.Ctx) error {
 	salary := c.Params("salary")
 
@@ -55,4 +65,92 @@ func CalculateEmployeeDeduction(c *fiber.Ctx) error {
 		fiber.Map{
 			"deduction": deduction,
 		})
+}
+
+func GetAllEmployeeDeductions(c *fiber.Ctx) error {
+	db, err := sql.Open("mysql", "root:2664@tcp(35.235.115.113:3306)/dev")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer db.Close()
+
+	rows, err := db.Query("SELECT * FROM get_employee_deductions;")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	deductions := []Deduction{}
+
+	for rows.Next() {
+		var deduction Deduction
+
+		err = rows.Scan(
+			&deduction.Period,
+			&deduction.Status,
+			&deduction.Id,
+			&deduction.Concept,
+			&deduction.Percentage,
+			&deduction.EffectiveDate,
+			&deduction.ExpirationDate,
+		)
+
+		if err != nil {
+			panic(err.Error())
+		}
+
+		deductions = append(deductions, deduction)
+	}
+
+	if err := rows.Err(); err != nil {
+		panic(err.Error())
+	}
+
+	return c.Status(fiber.StatusOK).JSON(deductions)
+}
+
+func GetAllEmployerDeductions(c *fiber.Ctx) error {
+	db, err := sql.Open("mysql", "root:2664@tcp(35.235.115.113:3306)/dev")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer db.Close()
+
+	rows, err := db.Query("SELECT * FROM get_employer_deductions;")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	deductions := []Deduction{}
+
+	for rows.Next() {
+		var deduction Deduction
+
+		err = rows.Scan(
+			&deduction.Period,
+			&deduction.Status,
+			&deduction.Id,
+			&deduction.Concept,
+			&deduction.Percentage,
+			&deduction.EffectiveDate,
+			&deduction.ExpirationDate,
+		)
+
+		if err != nil {
+			panic(err.Error())
+		}
+
+		deductions = append(deductions, deduction)
+	}
+
+	if err := rows.Err(); err != nil {
+		panic(err.Error())
+	}
+
+	return c.Status(fiber.StatusOK).JSON(deductions)
 }
